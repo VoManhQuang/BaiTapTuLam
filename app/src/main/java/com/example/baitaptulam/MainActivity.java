9e3299059ede3a798049459579ad4ab5bb270aba
VoManhQuang <vomanhquang0909@gmail.com>
2024-10-11 15:33:22 +0700

    j

---
+++ app/src/main/java/com/example/baitaptulam/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.baitaptulam;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+     FrameLayout frameLayout;
+     TabLayout  tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        frameLayout= findViewById(R.id.framelayout);
+        tabLayout= findViewById(R.id.tablayout);
+
+
+    }
+}
